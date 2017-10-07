01d42141151da28a0605c525ef2f175377b902d9
gavinandre <ga402159806@gmail.com>
2017-10-07 20:11:31 +0800

    add toolbar

---
+++ app/src/main/java/com/gavinandre/androidherosamples/chaptertwelve/toolbar/ToolbarActivity.java
@@ -0,0 +1,77 @@
+package com.gavinandre.androidherosamples.chaptertwelve.toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.gavinandre.androidherosamples.R;
+
+public class ToolbarActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ShareActionProvider mShareActionProvider;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        // 添加标题和图标
+        mToolbar.setLogo(R.mipmap.ic_launcher);
+        // 标题的文字需在setSupportActionBar之前，不然会无效
+        mToolbar.setTitle("主标题");
+        mToolbar.setSubtitle("副标题");
+        setSupportActionBar(mToolbar);
+
+        // 将Toolbar与DrawerLayout结合起来，实现android 5.X 中DrawerToggle状态变化的动态效果，
+        // 即最左边的Toggle在DrawerLayout滑动时，会从三条横线间变成一个箭头状的图案。
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, mToolbar,
+                R.string.abc_action_bar_home_description,
+                R.string.abc_action_bar_home_description_format);
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        // 菜单的监听可以在toolbar里设置，
+        // 也可通过Activity的onOptionsItemSelected回调方法来处理
+        mToolbar.setOnMenuItemClickListener(
+                new Toolbar.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_settings:
+                                Toast.makeText(ToolbarActivity.this,
+                                        "action_settings", Toast.LENGTH_SHORT).show();
+                                break;
+                            default:
+                                break;
+                        }
+                        return true;
+                    }
+                });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        /* ShareActionProvider配置 */
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menu
+                .findItem(R.id.action_share));
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/*");
+        mShareActionProvider.setShareIntent(intent);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
